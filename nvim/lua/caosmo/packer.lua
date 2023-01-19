fa6cb5f5ff51707a5c9bfa2161e2ff444493f7c8
yuri <yuri@vietcong.communist.org>
2023-01-19 09:09:14 -0300

    Agrego wallpaper y colorscheme

--- nvim/lua/caosmo/packer.lua
+++ nvim/lua/caosmo/packer.lua
@@ -11,9 +11,6 @@ return require('packer').startup(function(use)
 -- or                            , branch = '0.1.x',
   requires = { {'nvim-lua/plenary.nvim'} }
 }
-  use "rebelot/kanagawa.nvim"
-  use "EdenEast/nightfox.nvim"
-  use "arcticicestudio/nord-vim"
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('mbbill/undotree') 
   use('tpope/vim-fugitive')
@@ -22,6 +19,10 @@ return require('packer').startup(function(use)
   use('lervag/vimtex')
   use('christoomey/vim-tmux-navigator')
   use('jpalardy/vim-slime')
+  use "rebelot/kanagawa.nvim"
+  use "EdenEast/nightfox.nvim"
+  use "arcticicestudio/nord-vim"
+  use "folke/tokyonight.nvim"
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
